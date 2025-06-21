42e89ae289a945dd2c047a4a71fba5570d14b259
jaroud769 <jarod_games@hotmail.fr>
2025-06-21 16:18:13 +0200

    Ajout Classes
    ajout des classes, on redifinit plus ou moins tout en mieux on espère

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#Essayons de faire une simulation de la gravité en temps réel ce coup-ci
+#Importation des données
 
 import random
 import numpy as np
@@ -7,78 +7,57 @@ import random
 import matplotlib.pyplot as plt
 import pygame
 
-#On définit toutes nos fonctions 
-#Pour la compréhension : tab_r tableau contenant a tout instant pour tous corps toutes les positions, organisé comme suit :
-#bidimensionnel k indice du corps k , j indice de la direction (x,y en l'occurence mais on pourrait généraliser en 3D...) plein de vecteurs en colonnes
+########################################################################################
+# Définition des fonctions et classes 
 
-# Définir les constantes
-MASS = 1E3  # masse du vaisseau en kg (1 tonne)
-dF = 1E4 # Newton (force appliquée par l'utilisateur)
-dt = 3
-color = (255, 0, 255)  # couleur du vaisseau
-
-
-#on initialise nos tableaux
-n = 2 #Terre vaisseau
-tab_r=np.zeros((n,2)) #tableau des positions
-tab_v=np.zeros((n,2)) #tableau des vitesses
-
-#CI = [[[0,0],[5*1E4,-1E3]],[[1E7,1E7],[0,0]],[[-1E7,-1E7],[0,0]]]
-
-Rterre=149_597_870.7E3
-Rmars=227_944_000E3
-Vmars=0.1*24.080E3
-Vterre=29.7E3
-couleurs = {0:(0,0,255),1:(255,255,255),2:(255,0,0),3:(255,255,0)}
-
-#CI=[[[Rterre,0],[0,Vterre]],[[160E9,0],[0,0]],[[0,0],[0,0]],[[0,Rmars],[-Vmars,0]]]
-CI=[[[0,0],[0,0]],[[160E5,0],[0,0]],[[149_597_870.7E5,0],[29.7E3,0]],[[0,Rmars],[-Vmars,0]]]
-diameter_list=[100,1,10000]#100 et on a 7000km 
-liste_rayon = [7_000E3,10,700_000E3]
-               
-#Terre Vaisseau Soleil liste_masse =[5.972E24,1E3,10E26
-liste_masse=[5.9722E24,1E3,1.9891E30,6.417E23 ]#terre vaisseau, soleil mars
-
-liste_ac=[]#liste des accélérations pour chaque corps 
-G=6.67430E-11 #constante gravitationnelle
-dmin = 7000 #rayon du soleil taille limite (un peu arbitraire mais on s'en fout question d'ODG) car les corps ne sont pas ponctuels dans la vraie vie -> évite les divisions par 0
-
-def Force(rk,rj,k,j,liste_masse):#Calcule la force exercée par tous les corps j sur le cors k
+# Création de la classe des corps célestes
+class Corps:
+    def __init__(self,masse,distance,rayon,couleur,nom):
+        self.masse = masse
+        self.distance = distance
+        self.nom = nom
+        self.rayon = rayon
+        self.couleur = couleur
+    def caracteristiques(corps):
+        return(corps.masse,corps.distance,corps.rayon,corps.nom,corps.couleur)
     
+# Force Renvoie le vecteur force subit par le corps k 2 array
+def Force(rk,rj,k,j,liste_corps):#Calcule la force exercée par tous les corps j sur le cors k
+    
+    G=6.67430E-11 #constante gravitationnelle
+    rayon_k, _, masse_k = liste_corps[k].caracteristiques()
+    rayon_j, _, masse_j = liste_corps[j].caracteristiques()
     rkj_vec = rj-rk
     rkj = np.linalg.norm(rkj_vec)
-    if rkj < np.abs(liste_rayon[k]+liste_rayon[j]): # corps kj doivent être plus loins que les deux rayons cote a cote !
+    if rkj < (rayon_k + rayon_j): # évite les divergences
         return(np.array([0.0,0.0]))
-        
+      
     ukj=rkj_vec/rkj
-    
-    
 
-    return(np.array((G*liste_masse[k]*liste_masse[j]/(rkj**2))*ukj)) #retourne le vecteur force
-    
-def calc_acc_k(tab_r,k,liste_masse):#Calcule l'accélération du corps k au temps par PFD
+    return(np.array((G*masse_k*masse_j/(rkj**2))*ukj)) #retourne le vecteur force
 
-    close_index = []
+# calc_acc_k : Calcule l'accélération du corps k   
+def calc_acc_k(tab_r,k,liste_corps):
+
+    masse_k ,_ ,_ ,_= liste_corps[k].caracteristiques()
     n,d=np.shape(tab_r) # n nombres corps et d la dimenion x,y,z... 
-    rk=tab_r[k]  #vecteur position du corps k
-    Ftot=np.array([0.,0.]) #on initialise le vecteur force total des corps j sur k 
+    rk=tab_r[k,:]  #vecteur position du corps k
+    Ftot=np.array([0.0,0.0]) #on initialise le vecteur force total des corps j sur k 
         
     for j in range(n):#toutes les autres j planetes    
-        if j != k and liste_masse[k]/liste_masse[j] < 1E6 :  #conditions pour optimiser à verifier        
-            rj = tab_r[j] #vecteur position du corps j
-            Fkj = Force(rk,rj,k,j,liste_masse)
-            if Fkj == 0:
-                close_index.append(j)
+        if j != k and liste_masse[k]/liste_masse[j] < 1E6 :  #conditions pour optimiser à verifier    
+            masse_j ,_ ,_ ,_= liste_corps[j].caracteristiques()    
+            rj = tab_r[j,:] #vecteur position du corps j
+            Fkj = Force(rk,rj,k,j,liste_corps)
             Ftot += Fkj #on ajoute ttes les contributions des corps j      
             
     ak=Ftot/liste_masse[k]#on récupère l'accélération du corps ak cf PFD
 
-    return(ak,close_index)
-
+    return(ak)
 
 #Partie jeu 
-
-def controles():#Permet de gérer la force que l'on met !!!
+#controles : récupère les inputs du joueur
+def controles():
 
     # Contrôles
     
@@ -87,6 +66,8 @@ def controles():#Permet de gérer la force que l'on met !!!
     ds=0
     
     pressed = pygame.key.get_pressed()
+
+    # haut bas gauche droite
     if pressed[pygame.K_z]:
         dy -= 1  # vers le haut
     if pressed[pygame.K_s]:
@@ -95,25 +76,61 @@ def controles():#Permet de gérer la force que l'on met !!!
         dx -= 1  # gauche
     if pressed[pygame.K_d]:
         dx += 1  # droite
+
+    # Gestion du zoom/dezoom
     if pressed[pygame.K_a]:
         ds -= 0.01
     if pressed[pygame.K_e]:
         ds += 0.01
 
-    
-
-    
     return(dx,dy,ds)
 
-
-def rebond(v1,m1,v2,m2):#calcul du rebond élastique
+# rebond : calcul du rebond élastique
+def rebond(v1,m1,v2,m2):
     
     new_v1 = (2*m2/(m1+m2))*v2 + ((m1-m2)/(m1+m2))*v1
     new_v2 = (2*m2/(m1+m2))*v1 + ((m1-m2)/(m1+m2))*v2
 
     return(new_v1, new_v2)
+
+
+# Définition des constantes
+MASS = 1E3  # masse du vaisseau en kg (1 tonne)
+dF = 1E4 # Newton (force appliquée par l'utilisateur)
+dt = 3
+color = (255, 0, 255)  # couleur du vaisseau
+
+#on initialise nos tableaux
+
+n = 2 # nombre de corps 
+tab_r=np.zeros((2,n)) #tableau des positions
+tab_v=np.zeros((2,n)) #tableau des vitesses
+
+def CI_corps_k(liste_corps,k):
+
+    G=6.67430E-11 #constante gravitationnelle
+    theta = random.random()*2*np.pi
+    _,distance,_,_ = liste_corps[k].caracteristiques
+    if distance == 0 :
+        return()
+    x = distance * np.cos(theta)
+    y = distance * np.sin(theta) 
+    v_abs = np.sqrt( G * )
     
-#TESTONS la caméra qui suit le vaisseau 
+    
+#########################################################################################
+
+#Définitions des n corps !!!
+terre = Corps(5.9722E24,149_597_870.7E3,7_000E3,"Terre",(255,0,0))
+dist_vaisseau = 149_598_970.7E3 + 160E5 #on commence proche de la terre
+vaisseau = Corps(1E3,dist_vaisseau,10,"Vaisseau",(255,255,255))
+soleil = Corps(1.9884E30,0,700_000E3,"Soleil",(0,0,255))
+#Rmars=227_944_000E3
+#Vmars=0.1*24.080E3
+#Vterre=29.7E3
+        
+liste_ac=[]#liste des accélérations pour chaque corps 
+dmin = 7000 #rayon du soleil taille limite (un peu arbitraire mais on s'en fout question d'ODG) car les corps ne sont pas ponctuels dans la vraie vie -> évite les divisions par 0
 
 #boucle du jeu
 resolution=(2000, 1200)
@@ -149,7 +166,6 @@ while running:
 
     # Contrôles
     dx, dy, ds = controles()
-
     s += ds
     
     # Calcul de l'accélération en fonction de la force et de la masse
@@ -158,27 +174,23 @@ while running:
     a = [ax, ay]
     SCALE = 1E-4*10**s# échelle : 1 pixel = 10E4 km de larges
     
-    
     # Calculs des nouvelles positions des n corps !
     for k in range(n):
         
-            rk=tab_r[k] #vecteur position du corps k
-            vk=tab_v[k]
-            ak=liste_ac[k]
-            
-        
-            tab_r[k]=rk+dt*vk+0.5*(dt)**2*ak #on obtient toutes les nouvelles positions ce qui permet de calculer a_n+1
-        
+            rk=tab_r[k,:] #vecteur position du corps k
+            vk=tab_v[k,:]
+            ak=liste_ac[k,:]
+            tab_r[k,:]=rk+dt*vk+0.5*(dt)**2*ak #on obtient toutes les nouvelles positions ce qui permet de calculer a_n+1
             new_ak = calc_acc_k(tab_r,k,liste_masse)#on a la nouvelle accélération pour calculer new_v 
+
             if k == 1: #si on est le vaisseau on doit rajouter la propulsion !!
                 new_ak += a 
         
-        
-            tab_v[k]=tab_v[k]+0.5*(ak+new_ak)*dt#on obtient la nouvelle vitesse
-            liste_ac[k]=new_ak #on a besoin que de aik et new_aik pour calculer new_v on ne garde qu'une seule accélération en arrière
+            tab_v[k,:]=tab_v[k,:]+0.5*(ak+new_ak)*dt#on obtient la nouvelle vitesse
+            liste_ac[k,:]=new_ak #on a besoin que de aik et new_aik pour calculer new_v on ne garde qu'une seule accélération en arrière
         
     
-            pygame.draw.circle(screen, couleurs[k], (int((tab_r[k][0]-tab_r[1][0])*SCALE+CENTER[0]), int((tab_r[k][1]-tab_r[1][1])*SCALE+CENTER[1])), diameter_list[k]*SCALE*1E4)#on enleve la position du vaisseau changement de repère O->O' vaisseau, +res/2 on se met au centreE1
+            pygame.draw.circle(screen, couleurs[k], (int((tab_r[k,:][0]-tab_r[1,0])*SCALE+CENTER[0]), int((tab_r[k,1]-tab_r[1,1])*SCALE+CENTER[1])), diameter_list[k]*SCALE*1E4)#on enleve la position du vaisseau changement de repère O->O' vaisseau, +res/2 on se met au centreE1
             #pygame.draw.circle(screen, color, (int(1E6*SCALE+CENTER[0]), int(1E6*SCALE+CENTER[1])), 5)# on veut que a SCALE = 1E-4 diameter = 100 d'ou diameter = 100 *
             
     pygame.display.flip()
